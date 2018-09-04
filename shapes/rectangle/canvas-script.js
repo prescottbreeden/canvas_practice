68f7d767e527b6cc8a34b75fc963728850146a15
prescottbreeden <prescott.breeden@gmail.com>
2018-09-03 22:41:01 -0700

    rectangles

--- shapes/rectangle/canvas-script.js
+++ shapes/rectangle/canvas-script.js
@@ -20,16 +20,29 @@ window.onload = function() {
 	 
 	*/
 
+	// Rect
 	context.strokeStyle = 'red';
 	context.lineWidth = 11;
 	context.lineJoin = "round";
 	context.fillStyle = 'blue';
-
-	// Rect
 	context.rect(50,200,200,200);
 	context.stroke();
 	context.fill();
 
+	// fillRect
+	context.fillStyle = 'green';
+	context.fillRect(300, 200, 200, 200);
+
+	// strokeRect
+	context.lineWidth = 3;
+	context.strokeStyle = 'purple';
+	context.lineJoin = 'square';
+	context.strokeRect(550, 200, 200, 200);
+
+	context.clearRect(100, 100, 200, 200);
+
+	// clear canvas
+	// context.clearRect(0, 0, WIDTH, HEIGHT);
 
 
 }
